071e2a617af036a8a3b9781360f1e82788d9d0e4
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2026-05-18 23:33:13 +0400

    added favicon

--- ganaqro/ganaqro/urls.py
+++ ganaqro/ganaqro/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings            
-from django.conf.urls.static import static 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from projects.views.i18n_views import set_language
 
+FAVICON_URL = f'{settings.STATIC_URL}assets/img/favicon.jpg?v=1'
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=FAVICON_URL, permanent=False)),
     path(f'{settings.ADMIN_URL}', admin.site.urls),
     path('i18n/setlang/', set_language, name='set_language'),
     path('', include('projects.urls_v1')),
