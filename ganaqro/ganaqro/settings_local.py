740d27018c20f45c116113398c03a58820259e41
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2026-05-27 19:32:39 +0400

    fixed some bugs

--- ganaqro/ganaqro/settings_local.py
+++ ganaqro/ganaqro/settings_local.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv('')
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -192,11 +192,39 @@ CKEDITOR_CONFIGS = CKEDITOR_PROJECT_CONFIG
 
 
 # Email 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = os.getenv(
+    'EMAIL_BACKEND',
+    'django.core.mail.backends.smtp.EmailBackend',
+)
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() in ('true', '1', 'yes')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+CONTACT_RECEIVER_EMAIL = os.getenv(
+    'CONTACT_RECEIVER_EMAIL',
+    EMAIL_HOST_USER
+)
+
+# Lokal inkişaf: .env-də EMAIL yoxdursa settings_local-dakı SMTP istifadə et
+if DEBUG and not EMAIL_HOST_USER:
+    try:
+        from ganaqro import settings_local as _local_settings
+
+        EMAIL_BACKEND = _local_settings.EMAIL_BACKEND
+        EMAIL_HOST = _local_settings.EMAIL_HOST
+        EMAIL_PORT = _local_settings.EMAIL_PORT
+        EMAIL_HOST_USER = _local_settings.EMAIL_HOST_USER
+        EMAIL_HOST_PASSWORD = _local_settings.EMAIL_HOST_PASSWORD
+        EMAIL_USE_TLS = _local_settings.EMAIL_USE_TLS
+        EMAIL_USE_SSL = _local_settings.EMAIL_USE_SSL
+        DEFAULT_FROM_EMAIL = _local_settings.DEFAULT_FROM_EMAIL
+    except ImportError:
+        pass
